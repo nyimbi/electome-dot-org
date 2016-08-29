0c258a728dc7917e979e53cfe869652fcc88bfa7
annyuan <annyuan@Anns-MacBook-Pro.local>
2016-08-29 10:00:03 -0400

    add timeline shell

---
+++ src/routes/Home/components/Timeline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const clusters = require('../clusters.json')
+
+export const Timeline = React.createClass({
+	getInitialState() {
+		return {}
+	},
+
+	render() {
+		return (
+			<div>hello timeline</div>
+		)
+	}
+})
+
+export default Timeline
