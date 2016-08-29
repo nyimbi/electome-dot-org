5baf8caf5918bfb7c15e5e1e55a18200f369df04
annyuan <annyuan@anns-mbp.home>
2016-08-29 15:52:02 -0400

    render line

--- src/routes/Home/components/Timeline.js
+++ src/routes/Home/components/Timeline.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 const clusters = require('../clusters.json')
+let scene = null
 
 export const Timeline = React.createClass({
 	getInitialState() {
 		return {}
+	},
+
+	drawCurve() {
+		const subdivisions = 20
+
+		const points = [
+			new THREE.Vector3(10, 10, 10),
+			new THREE.Vector3(100, 100, 100),
+			new THREE.Vector3(50, 50, 50)
+		]
+
+		const spline = new THREE.Spline(points)
+		const geometrySpline = new THREE.Geometry()
+
+		for(let i=0; i<points.length * subdivisions; i++) {
+			let index = i / (points.length * subdivisions)
+			let position = spline.getPoint(index)
+
+			geometrySpline.vertices[ i ] = new THREE.Vector3( position.x, position.y, position.z )
+		}
+
+		geometrySpline.computeLineDistances()
+
+		const object = new THREE.Line( geometrySpline, new THREE.LineBasicMaterial({ color: 0xffffff }))
+
+		scene.add(object)
 	},
 
 	renderViz() {
@@ -12,23 +39,27 @@ export const Timeline = React.createClass({
 		const WIDTH = window.innerWidth
 		const HEIGHT = node.offsetHeight
 
-		const VIEW_ANGLE = 45
+		const VIEW_ANGLE = 60
 		const ASPECT = WIDTH / HEIGHT
-		const NEAR = 0.1
-		const FAR = 1000
+		const NEAR = 1
+		const FAR = 200
 
 		const renderer = new THREE.WebGLRenderer()
 		const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR)
 
-		const scene = new THREE.Scene()
+		scene = new THREE.Scene()
 
 		scene.add(camera)
 
-		camera.position.z = 300
+		camera.position.z = 150
 
 		renderer.setSize(WIDTH, HEIGHT)
 
 		node.appendChild(renderer.domElement)
+
+		this.drawCurve()
+
+		renderer.render(scene, camera)
 	},
 
 	componentDidMount() {
