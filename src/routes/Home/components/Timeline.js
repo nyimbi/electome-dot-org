442038460938ea04a3a7b2da398bf078311cada2
annyuan <annyuan@Anns-MacBook-Pro.local>
2016-08-29 13:01:45 -0400

    setup

--- src/routes/Home/components/Timeline.js
+++ src/routes/Home/components/Timeline.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 const clusters = require('../clusters.json')
 
 export const Timeline = React.createClass({
@@ -6,9 +7,37 @@ export const Timeline = React.createClass({
 		return {}
 	},
 
+	renderViz() {
+		const node = ReactDOM.findDOMNode(this)
+		const WIDTH = window.innerWidth
+		const HEIGHT = node.offsetHeight
+
+		const VIEW_ANGLE = 45
+		const ASPECT = WIDTH / HEIGHT
+		const NEAR = 0.1
+		const FAR = 1000
+
+		const renderer = new THREE.WebGLRenderer()
+		const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR)
+
+		const scene = new THREE.Scene()
+
+		scene.add(camera)
+
+		camera.position.z = 300
+
+		renderer.setSize(WIDTH, HEIGHT)
+
+		node.appendChild(renderer.domElement)
+	},
+
+	componentDidMount() {
+		setTimeout(this.renderViz, 100)
+	},
+
 	render() {
 		return (
-			<div>hello timeline</div>
+			<div className="timeline"></div>
 		)
 	}
 })
