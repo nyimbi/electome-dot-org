a2b7e507031a91101ecd45cb4ee5b49f7e8e7442
annyuan <annyuan@Anns-MacBook-Pro.local>
2016-08-30 16:46:08 -0400

    add components

--- src/routes/Home/components/VideoPlayer.js
+++ src/routes/Home/components/VideoPlayer.js
@@ -2,16 +2,25 @@ import React from 'react'
 
 export const VideoPlayer = React.createClass({
 	getInitialState() {
-		return {}
+		return {
+			videoShowing: false
+		}
 	},
 
 	render() {
 		return (
-			<div className="video-player">
+			<div data-video-showing={this.state.videoShowing} className="video-player">
 				<div className="iframe-wrapper">
+					<div className="close" onClick={() => {
+						this.setState({ videoShowing: false })
+					}}>
+						<i className="material-icons">clear</i>
+					</div>
 					<iframe width="420" height="315" src="https://www.youtube.com/embed/g8gJOCwBuFc" frameborder="0" allowfullscreen></iframe>
 				</div>
-				<div className="viewdemo">View demo</div>
+				<div onClick={() => {
+					this.setState({ videoShowing: true })
+				}} className="viewdemo">View demo</div>
 			</div>
 		)
 	}
