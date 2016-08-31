1e61eeeb63ead2014df3ec3559e7aacaccd6df7d
annyuan <annyuan@vpn-i.media.mit.edu>
2016-08-31 07:36:55 -0400

    naively render

--- src/routes/Home/components/Timeline2D.js
+++ src/routes/Home/components/Timeline2D.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import moment from 'moment'
+
+const clusters = require('../clusters.json')
 
 export const Timeline = React.createClass({
+	componentDidMount() {
+
+	},
+
 	render() {
 		return (
-			<div className="timeline"></div>
+			<div className="timeline">
+				<div className="events">
+					{clusters.map((c, i) => {
+						const words = c.top_scoring_terms.map(w =>
+							<div key={w[0]} className="word">{w[0]}</div>)
+						return <div key={i} className="event">
+							{words}
+							<div>{c.headline_tweet.tweet}</div>
+						</div>
+					})}
+				</div>
+			</div>
 		)
 	}
 })
